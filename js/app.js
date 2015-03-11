8a26b8832ddead6e608079e2cd8a8920ff2f27e3
Khaleel Jageer <jskcse4@gmail.com>
2015-03-11 09:45:22 +0530

    Khaleel's Commit

---
+++ js/app.js
@@ -0,0 +1,99 @@
+//lists
+document.querySelector('#btn-lists1').addEventListener ('click', function () {
+  document.querySelector('#lists1').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back1').addEventListener ('click', function () {
+  document.querySelector('#lists1').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists2').addEventListener ('click', function () {
+  document.querySelector('#lists2').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back2').addEventListener ('click', function () {
+  document.querySelector('#lists2').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists3').addEventListener ('click', function () {
+  document.querySelector('#lists3').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back3').addEventListener ('click', function () {
+  document.querySelector('#lists3').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists4').addEventListener ('click', function () {
+  document.querySelector('#lists4').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back4').addEventListener ('click', function () {
+  document.querySelector('#lists4').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists5').addEventListener ('click', function () {
+  document.querySelector('#lists5').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back5').addEventListener ('click', function () {
+  document.querySelector('#lists5').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists6').addEventListener ('click', function () {
+  document.querySelector('#lists6').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back6').addEventListener ('click', function () {
+  document.querySelector('#lists6').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists7').addEventListener ('click', function () {
+  document.querySelector('#lists7').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back7').addEventListener ('click', function () {
+  document.querySelector('#lists7').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists8').addEventListener ('click', function () {
+  document.querySelector('#lists8').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back8').addEventListener ('click', function () {
+  document.querySelector('#lists8').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists9').addEventListener ('click', function () {
+  document.querySelector('#lists9').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back9').addEventListener ('click', function () {
+  document.querySelector('#lists9').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//lists
+document.querySelector('#btn-lists10').addEventListener ('click', function () {
+  document.querySelector('#lists10').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-lists-back10').addEventListener ('click', function () {
+  document.querySelector('#lists10').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
